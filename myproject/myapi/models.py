b30959e1bc60fcef3483a0bb39db8e4f5c808fcf
matthoot <matt221136@gmail.com>
2024-03-28 15:30:23 -0400

    created django models for database

--- myproject/myapi/models.py
+++ myproject/myapi/models.py
@@ -1,3 +1,35 @@
+
 from django.db import models
 
 # Create your models here.
+# Make sure to run 'python manage.py makemigrations' and 'python manage.py migrate' after making any changes to these models.
+
+class Account(models.Model):
+    user_name = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+
+class LocationType(models.Model):
+    location_type = models.CharField(max_length=255, unique=True)
+
+class Location(models.Model):
+    location_type = models.ForeignKey(LocationType, on_delete=models.CASCADE)
+    location_name = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=10)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    street_address = models.CharField(max_length=255)
+    hours_of_op = models.CharField(max_length=255, null=True)
+    reviews_per_star = models.IntegerField(null=True)
+    average_star_rating = models.FloatField(null=True)
+
+class SavedRoute(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    zip_code = models.CharField(max_length=10)
+
+class SavedRouteLocation(models.Model):
+    route = models.ForeignKey(SavedRoute, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    location_order = models.IntegerField()
+
+
