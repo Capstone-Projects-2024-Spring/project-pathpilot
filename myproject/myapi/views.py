4260d746b96faa82afe2346956096f4319157be8
matthoot <matt221136@gmail.com>
2024-04-02 10:45:43 -0400

    login successful with super user

--- myproject/myapi/views.py
+++ myproject/myapi/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.http import JsonResponse
 from django.contrib.auth import authenticate, login
+from myapi.models import Account
 
 @api_view(['GET'])
 def hello_world(request):
@@ -11,10 +12,16 @@ def hello_world(request):
 @api_view(['POST'])
 def login_view(request):
     if request.method == 'POST':
-        print("here")
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
+        username = request.data.get('username')
+        password = request.data.get('password')
+        print("username: ", username)
+        print("password: ", password)
+
+        get_all_accounts()
+        check_credentials(username, password)
+        #check_credentials("temple", "temple123")
+        user = authenticate(username=username, password=password)
+        print("user = ", user)
 
         if user is not None:
             login(request, user)
@@ -23,3 +30,31 @@ def login_view(request):
             return JsonResponse({'error': 'Invalid username or password'}, status=400)
     else:
         return JsonResponse({'error': 'Method not allowed'}, status=405)
+
+
+
+def check_credentials(username, password):
+    try:
+        # Retrieve the account based on the provided username
+        account = Account.objects.get(user_name=username)
+        
+        # Check if the provided password matches the password stored in the database
+        if account.password == password:
+            print("Credentials match!")
+        else:
+            print("Invalid password!")
+    except Account.DoesNotExist:
+        print("Account does not exist!")
+
+def get_all_accounts():
+    # Retrieve all accounts from the database
+    all_accounts = Account.objects.all()
+
+    # Iterate over the queryset and print each account's details
+    for account in all_accounts:
+        # print(f"Account ID: {account.id}")
+        # print(f"Email: {account.email}")
+        print("-------------------")
+        print(f"Username: {account.user_name}")
+        print(f"password: {account.password}")
+        print("-------------------")
