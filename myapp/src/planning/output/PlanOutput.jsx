0c96f7fc5648d55383f2a7126811537dd995c6b9
eross04 <erinross@temple.edu>
2024-04-03 16:35:55 -0400

    Added Philly Google Maps to planning page

--- myapp/src/planning/output/PlanOutput.jsx
+++ myapp/src/planning/output/PlanOutput.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const PlanOutput = ({ locations, path }) => {
+    let map;
+
+    useEffect(() => {
+        async function initMap() {
+            const google = window.google;
+            const { Map } = await google.maps.importLibrary("maps");
+        
+            map = new Map(document.getElementById("map"), {
+                center: { lat: 39.9526, lng: -75.1652 },
+                zoom: 14,
+            });
+        }
+      initMap();
+    }, []);
+    
 
     return (
-        <div>
-            <h1>This is a test2</h1>
+        <div className='output-padding'>
+            <div id="map"></div>
         </div>
     )
 };
