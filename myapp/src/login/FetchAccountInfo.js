b2afbe13ca20a61340fc21e09bc9bf9b5bb9c97b
matthoot <matt221136@gmail.com>
2024-04-05 19:17:02 -0400

    removed console log

--- myapp/src/login/FetchAccountInfo.js
+++ myapp/src/login/FetchAccountInfo.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 import { loginURL } from '../api/apiConfig.js';
 
 export const FetchAccountInfo = async (username, password) => {
-    console.log("in fetch account info function")
-    console.log("login URL = ",loginURL)
 
     try {
         const response = await axios.post(`${loginURL}`, {
