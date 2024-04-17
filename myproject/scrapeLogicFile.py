b3e113e2f4ad87ce5d45c05b7f7200740c62f2c2
Margaret Hanley <tuj92824@temple.edu>
2024-04-17 14:37:06 -0400

    More data!
    added restaurants and coffee shops focused in center city, 19103 zipcode

--- myproject/scrapeLogicFile.py
+++ myproject/scrapeLogicFile.py
@@ -19,13 +19,13 @@ from bs4 import BeautifulSoup
 from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut, GeocoderServiceError
 
-PLACETYPE = "parks"
-ZIPCODEINPUT = 19122
+PLACETYPE = "coffee+shops"
+ZIPCODEINPUT = 19103
 insideURLArray = []
 finalRestaurantList = [None] * 10
 #restaurantList = []
 def createURL(zipcode, loType):
-    url = f"https://www.yelp.com/search?find_desc={loType}&find_loc=Philadelphia%2C+PA+{zipcode}&cflt=parks"
+    url = f"https://www.yelp.com/search?find_desc={loType}&find_loc=Philadelphia%2C+PA+{zipcode}&cflt=coffee"
     print(loType)
     return url
 
@@ -361,7 +361,7 @@ def main():
             print("Longitude: " + str(longitude))
             addtoDatabase(value)
     print('Here we go')
-    while(numb<=9 and numb>=1): #cap at 300 to be safe, unlikely beyond that, program just stops when it cant reach site anymore
+    while(numb<=30 and numb>=1): #cap at 300 to be safe, unlikely beyond that, program just stops when it cant reach site anymore
         #change up to number based on needs
         val = numb*10
         tempUrl= url + f"&start={val}"
