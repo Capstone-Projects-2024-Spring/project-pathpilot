5018fc8f5b3ff36625f9382ea69ceee1c4780415
eross04 <erinross@temple.edu>
2024-04-22 16:48:11 -0400

    Add location types on frontend

--- myapp/src/planning/input/LocationTypes.js
+++ myapp/src/planning/input/LocationTypes.js
@@ -1,11 +1,16 @@
 /*Eventually put in a function here to call the backend to get the list*/
 
 const LocationTypes = [
-    {value: '2', label: 'Coffee Shop'},
-    {value: '3', label: 'Museum'},
-    {value: '5', label: 'Ice Cream Shop'},
-    {value: '4', label: 'Book Store'},
-    {value: '1', label: 'Restaurant'}
+    {value: 2, label: 'Coffee Shop'},
+    {value: 3, label: 'Museum'},
+    {value: 5, label: 'Ice Cream Shop'},
+    {value: 4, label: 'Book Store'},
+    {value: 1, label: 'Restaurant'},
+    {value: 6, label: 'Playground'},
+    {value: 7, label: 'Park'},
+    {value: 8, label: 'Vintage Shop'},
+    {value: 9, label: 'Bakery'},
+    {value: 10, label: 'Activity'}
 ]
 
 export {LocationTypes};
