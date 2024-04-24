795561efe3e5abf54ffc719bb97e6e11e570bea5
Margaret Hanley <tuj92824@temple.edu>
2024-04-24 11:46:52 -0400

    new bagel tag

--- myproject/scrapeLogicFile.py
+++ myproject/scrapeLogicFile.py
@@ -19,7 +19,7 @@ from bs4 import BeautifulSoup
 from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut, GeocoderServiceError
 
-PLACETYPE = "coffee+shops"
+PLACETYPE = "bakeries"
 ZIPCODEINPUT = 19146
 insideURLArray = []
 finalRestaurantList = [None] * 10
@@ -301,10 +301,6 @@ def addtoDatabase(infoDict):
             address = infoDict["address"][0]  + " " + infoDict["address"][1] + " " + infoDict["address"][2] + " " + infoDict["address"][3]
             latitude = infoDict["address"][4]
             longitude = infoDict["address"][5]
-    elif(len(infoDict["address"])==1 and infoDict["address"][0] != -1): # JUST AN ADDRESS
-        address = infoDict["address"][0]
-        latitude = -1
-        longitude =-1
     else:
         address=-1
         latitude = infoDict["address"][0]
